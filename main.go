4c55a44eceb7ad9b03b4aa5876f04210b6850d38
Nishant Srivastava <nishant@rsrivastava.io>
2025-01-13 13:58:14 -0500

    initial update

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	session, error := discordgo.New("Bot " + os.Getenv("DISCORD_BOT_TOKEN"))
+
+	if error != nil {
+		log.Fatal("Error creating Discord session: ", error)
+		return
+	}
+
+	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "ping" {
+			s.ChannelMessageSend(m.ChannelID, "Pong!")
+		}
+	})
+
+}
